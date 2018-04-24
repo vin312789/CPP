1db17e8b5693eef5d3b3c69ec9af18c3e60bb223
MyDearGreatTeacher <37649784+MyDearGreatTeacher@users.noreply.github.com>
2018-04-24 14:08:34 +0800

    Add files via upload

---
+++ 20180424/TestCircle.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+class Circle
+{
+public:
+  // The radius of this circle
+  double radius;
+
+  // Construct a default circle object
+  Circle()
+  {
+    radius = 1;
+  }
+
+  // Construct a circle object
+  Circle(double newRadius)
+  {
+    radius = newRadius;
+  }
+
+  // Return the area of this circle
+  double getArea()
+  {
+    return radius * radius * 3.14159;
+  }
+};  // Must place a semicolon here
+
+int main()
+{
+  Circle circle1(1.0);
+  Circle circle2(25);
+  Circle circle3(125);
+
+  cout << "The area of the circle of radius "
+    << circle1.radius << " is " << circle1.getArea() << endl;
+  cout << "The area of the circle of radius "
+    << circle2.radius << " is " << circle2.getArea() << endl;
+  cout << "The area of the circle of radius "
+    << circle3.radius << " is " << circle3.getArea() << endl;
+
+  // Modify circle radius
+  circle2.radius = 100;
+  cout << "The area of the circle of radius "
+    << circle2.radius << " is " << circle2.getArea() << endl;
+
+  return 0;
+}
